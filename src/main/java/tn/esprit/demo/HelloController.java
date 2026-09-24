2a0787391502a1439c66ffaf6b25f54a9a57b0ed
Moamei <amri.mohamed2004@gmail.com>
2026-09-24 10:57:46 +0200

    first commit

---
+++ src/main/java/tn/esprit/demo/HelloController.java
@@ -0,0 +1,10 @@
+package tn.esprit.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Bonjour Spring Boot";
+    }
+}
